fab4cc67a155198e1b0dba5a17fa5f81c9874e8a
bl0nder <goelpalaash@gmail.com>
2022-12-21 13:57:22 +0530

    .

--- kernelModule.c
+++ kernelModule.c
@@ -1,40 +1,58 @@
-#include <linux/module.h>
 #include <linux/kernel.h>
-#include <linux/sched.h>
+#include <linux/module.h>
 #include <linux/pid.h>
 #include <linux/pid_namespace.h>
+#include <linux/moduleparam.h>
+#include <linux/sched.h>
 #include <linux/cred.h>
 
-static int pidInt;
-static struct pid *pidStruct;
-struct task_struct *curTask;
+MODULE_AUTHOR("Palaash Goel");
+MODULE_LICENSE("GPL");
 
-module_param(pidInt, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+static int pNum;
+static struct task_struct* pTask;
+static struct pid* pidStruct;
+
+module_param(pNum, int, 666);
 
 int init_module(void) {
-    pidStruct = find_get_pid(pidInt);
-    if (pidStruct==NULL) {
-        pr_info("Invalid PID\n");
+    if ((pidStruct = find_get_pid(pNum)) == NULL) {
+        pr_info("NULL PID found. Terminating.\n");
+        return 0;
+    }
+    
+
+    if ((pTask = pid_task(pidStruct, PIDTYPE_PID)) == NULL) {
+        pr_info("NULL task struct found. Terminating.\n");
         return 0;
     }
 
-    curTask = pid_task(pidStruct, PIDTYPE_PID);
-    if (curTask==NULL) {
-        pr_info("Unexpected error occured.\n");
-        return 0;
-    }
+    pr_info("Process Name: %s\n", pTask -> comm);
+    pr_info("PID: %d\n", pTask -> pid);
+    pr_info("UID: %d\n", pTask -> cred -> uid);
+    pr_info("PGID: %d\n", pTask -> cred -> gid);
+    pr_info("Process Name: %s\n", pTask -> comm);
+    //Getting executable path
+    // char* path = malloc(1000);
 
-    pr_info("PID: %d\n", curTask->pid);
-    pr_info("User ID: %d\n", curTask->cred->uid);
-    pr_info("Process group ID: %d\n", pid_vnr(task_pgrp(curTask))); //task_tgid_nr(current)
-    pr_info("Process command path: %s\n", curTask->comm);
+    // struct mm_struct* mm = pTask -> mm;
 
+    // if (mm != NULL) {
+    //     down_read(&mm->mmap_base);
+    //     if (mm->exe_file != NULL) 
+    //     {
+    //         strcpy(path, mm -> exe_file -> f_path -> d_name, 1000);           
+    //     }
+    //     up_read(&mm->mmap_base);
+    // } 
+
+    // printk(KERN_INFO "Command path: %s\n", path);
     return 0;
 }
 
-
-void cleanup_module(void) {
-    pr_info("Done\n");
+void cleanup_module() {
+    printk(KERN_INFO "Cleaning module has run.\n");
 }
 
-MODULE_LICENSE("GPL");
+
+
