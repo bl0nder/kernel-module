ba6bd9e487b919e8405bfb86ecbf475b9e3edc2a
bl0nder <goelpalaash@gmail.com>
2022-12-21 22:42:55 +0530

    .

--- myModule.c
+++ myModule.c
@@ -17,21 +17,21 @@ module_param(pNum, int,  S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
 int init_module(void) {
     if (!(pidStruct = find_get_pid(pNum))) {
-        pr_info("NULL PID found. Terminating.\n");
+        prink(KERN_INFO "NULL PID found. Terminating.\n");
         return 0;
     }
     
 
     if (!(pTask = pid_task(pidStruct, PIDTYPE_PID))) {
-        pr_info("NULL task struct found. Terminating.\n");
+        printk(KERN_INFO "NULL task struct found. Terminating.\n");
         return 0;
     }
 
-    pr_info("Process Name: %s\n", pTask -> comm);
-    pr_info("PID: %d\n", pTask -> pid);
-    pr_info("UID: %d\n", pTask -> cred -> uid);
-    pr_info("PGID: %d\n", pTask -> cred -> gid);
-    pr_info("Command Path: %s\n", pTask -> comm);
+    printk(KERN_INFO "Process Name: %s\n", pTask -> comm);
+    printk(KERN_INFO "PID: %d\n", pTask -> pid);
+    printk(KERN_INFO "UID: %d\n", pTask -> cred -> uid);
+    printk(KERN_INFO "PGID: %d\n", pTask -> cred -> gid);
+    printk(KERN_INFO "Command Path: %s\n", pTask -> comm);
     //Getting executable path
     // char* path = malloc(1000);
 
@@ -51,5 +51,5 @@ int init_module(void) {
 }
 
 void cleanup_module(void) {
-    printk(KERN_INFO "Cleaning module has run.\n");
+    printk(KERN_INFO "Module has been removed and cleanup module has run.\n");
 }
