ed3ebb1d97c6822009f89a7a62f59d531e74cd8e
bl0nder <goelpalaash@gmail.com>
2022-12-21 13:47:27 +0530

    .

--- kernelModule.c
+++ kernelModule.c
@@ -29,22 +29,22 @@ int init_module(void) {
     printk(KERN_INFO "PID: %d\n", pTask -> pid);
     printk(KERN_INFO "UID: %d\n", pTask -> cred -> uid);
     printk(KERN_INFO "PGID: %d\n", pTask -> cred -> gid);
-    
+    printk(KERN_INFO "Process Name: %s\n", pTask -> comm);
     //Getting executable path
-    char* path = malloc(1000);
+    // char* path = malloc(1000);
 
-    struct mm_struct* mm = pTask -> mm;
+    // struct mm_struct* mm = pTask -> mm;
 
-    if (mm != NULL) {
-        down_read(&mm->mmap_base);
-        if (mm->exe_file != NULL) 
-        {
-            strcpy(path, mm -> exe_file -> f_path -> d_name, 1000);           
-        }
-        up_read(&mm->mmap_base);
-    } 
+    // if (mm != NULL) {
+    //     down_read(&mm->mmap_base);
+    //     if (mm->exe_file != NULL) 
+    //     {
+    //         strcpy(path, mm -> exe_file -> f_path -> d_name, 1000);           
+    //     }
+    //     up_read(&mm->mmap_base);
+    // } 
 
-    printk(KERN_INFO "Command path: %s\n", path);
+    // printk(KERN_INFO "Command path: %s\n", path);
     return 0;
 }
 
